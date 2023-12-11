593df17af6c67cb122f600834089f8b38bfb81e8
Vishwa prasanth <vishwaprasanth1411@gmail.com>
2023-12-11 10:34:08 +0530

    chakra ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Button } from '@chakra-ui/react'
+
 function App() {
  
 
   return (
     <>
-    hi
+    <Button>
+      Hello button
+    </Button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const styles = {
+  global:(props) => ({
+    body:{
+      
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme = {theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
